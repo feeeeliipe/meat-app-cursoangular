22cdbfc00869d3a388abe2b10f934589a4b65cd1
feeeeliipe <feeeeliipe>
2019-05-25 15:13:49 -0300

    Implementando componente de avaliacoes

--- src/app/restaurant-detail/reviews/reviews.component.ts
+++ src/app/restaurant-detail/reviews/reviews.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { RestaurantService } from '../../restaurants/restaurants.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'mt-reviews',
@@ -6,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReviewsComponent implements OnInit {
 
-  constructor() { }
+  reviews: Observable<any>;  
+
+
+  constructor(private service: RestaurantService,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
+      this.reviews = this.service
+                         .reviewsOfRestaurant(
+                           this.route
+                               .parent
+                               .snapshot
+                               .params['id']);
   }
 
 }
