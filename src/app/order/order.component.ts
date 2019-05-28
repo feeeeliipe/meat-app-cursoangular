f2de4e4e928204ce3a30400bb2664d72c9346320
feeeeliipe <feeeeliipe>
2019-05-28 20:26:28 -0300

    Utilizando validadores em Reactive Forms

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -4,9 +4,9 @@ import { OrderService } from '../order/order.service';
 import { CartItem } from 'app/restaurant-detail/shopping-cart/cart-item.model';
 import { Order, OrderItem } from './order.model';
 import { Router } from '@angular/router';
-import { routerNgProbeToken } from '@angular/router/src/router_module';
 
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
+
 
 @Component({
   selector: 'mt-order',
@@ -16,8 +16,12 @@ import { FormGroup, FormBuilder } from '@angular/forms';
 export class OrderComponent implements OnInit {
 
   orderForm: FormGroup;
-
   delivery: number = 8;
+
+  emailPattern = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+  numberPattern = /^[0-9]*$/;
+
+
 
   paymentOptions: RadioOption[] = [
     {label:'Dinheiro', value:'MONEY'},
@@ -31,14 +35,29 @@ export class OrderComponent implements OnInit {
 
   ngOnInit() {
     this.orderForm = this.formBuilder.group({
-      name: this.formBuilder.control(''),
-      email: this.formBuilder.control(''),
-      emailConfirmation: this.formBuilder.control(''),
-      address: this.formBuilder.control(''),
-      number: this.formBuilder.control(''),
+      name: this.formBuilder.control('', [Validators.required, Validators.minLength(5)]),
+      email: this.formBuilder.control('', [Validators.required, Validators.pattern(this.emailPattern)]),
+      emailConfirmation: this.formBuilder.control('', [Validators.required, Validators.pattern(this.emailPattern)]),
+      address: this.formBuilder.control('', [Validators.required, Validators.minLength(5)]),
+      number: this.formBuilder.control('', [Validators.required, Validators.pattern(this.numberPattern)]),
       optionalAddress: this.formBuilder.control(''),
-      paymentOption: this.formBuilder.control('')
-    })
+      paymentOption: this.formBuilder.control('', [Validators.required])
+    }, {validator: OrderComponent.equalsTo})
+  }
+  
+  static equalsTo(group: AbstractControl): {[key:string]: boolean} {
+    const email = group.get('email');
+    const emailConfirmation = group.get('emailConfirmation');
+
+    if (!email || !emailConfirmation) {
+      return undefined;
+    }
+
+    if (email.value != emailConfirmation.value) {
+      return {emailsNotMatch:true};
+    }
+
+    return undefined;
   }
   
   itemsValue(): number {
