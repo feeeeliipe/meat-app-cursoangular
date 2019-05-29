d5585052addf42789ddc342e204dd4eaf0828084
feeeeliipe <feeeeliipe>
2019-05-29 19:07:16 -0300

    Criando novos modulos e implementando Lazy Loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,6 @@
 import {Routes} from '@angular/router'
 import { HomeComponent } from './home/home.component'
-import { AboutComponent } from './about/about.component'
 import { RestaurantsComponent } from './restaurants/restaurants.component';
-import { componentFactoryName } from '@angular/compiler';
-import { RestaurantComponent } from './restaurants/restaurant/restaurant.component';
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
 import { MenuComponent } from './restaurant-detail/menu/menu.component';
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
@@ -12,7 +9,6 @@ import { OrderSummaryComponent } from './order-summary/order-summary.component';
 
 export const ROUTES: Routes = [
     {path:'', component: HomeComponent},
-    {path:'about', component: AboutComponent},
     {path:'restaurants', component: RestaurantsComponent},
     {path:'restaurants/:id', component: RestaurantDetailComponent,
         children: [
@@ -22,5 +18,9 @@ export const ROUTES: Routes = [
         ]
     },
     {path:'order', component: OrderComponent},
-    {path:'order-summary', component: OrderSummaryComponent}
+    {path:'order-summary', component: OrderSummaryComponent},
+
+    // LAZY LOADING 
+    {path:'about', loadChildren: './about/about.module#AboutModule'}
+    
 ]
