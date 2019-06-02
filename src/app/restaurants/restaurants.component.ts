d0f68a994fcf7faf59a51078a62ce3bd160bef4c
feeeeliipe <feeeeliipe>
2019-06-02 18:59:11 -0300

    Tratando exceções na busca

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -4,9 +4,12 @@ import { RestaurantService } from './restaurants.service';
 import { trigger, state, style, transition, animate } from '@angular/animations'
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms'
 import 'rxjs/add/operator/switchMap';
-import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/from';
+
+import { Observable }from 'rxjs/Observable';
 
 @Component({
   selector: 'mt-restaurants',
@@ -59,7 +62,6 @@ export class RestaurantsComponent implements OnInit {
     this.searchControl.valueChanges
         .debounceTime(500)
         .distinctUntilChanged()
-        .do(searchTerm => console.log('q = ' + searchTerm))
         .switchMap(searchTerm => 
           this.restaurantService.restaurants(searchTerm))
           .subscribe(restaurants => this.restaurants = restaurants)
